27ebc55c9357b25d3fd7c7973d839dd5830f7f48
Tugba Esat <tugbaesat007@gmail.com>
2023-04-10 17:14:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+let a = 10;
+let b = 5;
+// function add() {
+//     return (a + b);
+//   }
+// function subtract() {
+//     return (a - b);
+//   }
+// function multiply() {
+//     return (a * b);
+//   }
+// function divide() {
+//     return (a / b);
+//   }
+  function add(a, b) {
+    return (a + b);
+  }
+function subtract(a, b) {
+    return (a - b);
+  }
+function multiply(a, b) {
+    return (a * b);
+  }
+function divide(a, b) {
+    return (a / b);
+  }
+function increment(n) {
+  n++;  
+  return n;
+}
+function decrement(n) {
+    n--;  
+    return n; 
+  }
+
+function makeInt(n) {
+    return parseInt(n, 10);
+
+  }
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
